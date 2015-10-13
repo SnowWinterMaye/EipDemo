5a974b31ca45fbff9bc824e53feb7f5e4c9798d2
SnowWinterMaye <cxdmaye@hotmail.com>
2015-10-13 23:07:26 +0800

    kk

--- common/js/app.js
+++ common/js/app.js
@@ -1,17 +1,38 @@
-var hys = angular.module('hysapp', ['ionic']);
+var hys = angular.module('hysapp', ['ngRequire','ionic']);
 var lastroute = "";
 hys
-    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    .config(['$stateProvider', '$urlRouterProvider', '$requireProvider',
+        function ($stateProvider, $urlRouterProvider,$requireProvider) {
         $stateProvider
-            .state('/', {
-                url: '/',
-                templateUrl: '/',
-                controller: 'indexCtrl'
-            }).state('/homepage', {
+           .state('/homepage', {
                 url: '/homepage',
                 templateUrl: 'p_homepage/homepage.html',
                 controller: 'homepageCtrl'
-            });
+            }).state('/applyform', {
+                url: '/applyform',
+                templateUrl: 'p_applyformpage/applyform.html',
+                controller: 'applyformCtrl',
+                // resolve:{
+                //     deps:$requireProvider.requireJS([
+                //         'a_applyformpage/p_apartpage/apartCtrl',
+                //         'a_applyformpage/p_projectpage/projectCtrl'
+                //     ])
+                //     // ,
+                //     // css:$requireProvider.requireCSS([
+                        
+                //     // ])
+                // }
+            })
+            // .state('/apart', {
+            //     url: '/apart',
+            //     templateUrl: 'p_applyformpage/p_apartpage/apart.html',
+            //     controller: 'apartCtrl'
+            // }).state('/project', {
+            //     url: '/project',
+            //     templateUrl: 'p_applyformpage/p_projectpage/project.html',
+            //     controller: 'projectCtrl'
+            // })
+            ;
         var D = new Date();
         console.log("Staten:" + D.getTime());
         $urlRouterProvider.otherwise('/homepage');
--- p_applyformpage/applyformCtrl.js
+++ p_applyformpage/applyformCtrl.js
@@ -1,45 +1,48 @@
 hys
-.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) { 
-    $stateProvider
-   .state('/applyform', {
-                url: '/applyform',
-                templateUrl: 'p_applyformpage/applyform.html',
-                controller: 'applyformCtrl'
-            }).state('/apart', {
-                url: '/apart',
-                templateUrl: 'p_applyformpage/p_apartpage/apart.html',
-                controller: 'apartCtrl'
-            }).state('/project', {
-                url: '/project',
-                templateUrl: 'p_applyformpage/p_projectpage/project.html',
-                controller: 'projectCtrl'
-            });
-             $urlRouterProvider.otherwise('/applyform');
-}])
-.controller('applyformCtrl', function ($scope, $state) {
-    $('body').append("<script src='p_applyformpage/p_apartpage/apartCtrl.js'></script>");
-    $('body').append("<script src='p_applyformpage/p_projectpage/projectCtrl.js'></script>");
-    // document.write("<script src='p_applyformpage/p_apartpage/apartCtrl.js'></script>");
-    // document.write("<script src='p_applyformpage/p_projectpage/projectCtrl.js'></script>");
-    $scope.greet = "申请填报主界面";
-    //返回主界面
-    $scope.gohome = function () {
-        $state.go('/homepage');
-        
-    }
+    // .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    //     $stateProvider
+    //         .state('/applyform', {
+    //             url: '/applyform',
+    //             templateUrl: 'p_applyformpage/applyform.html',
+    //             controller: 'applyformCtrl'
+    //         }).state('/apart', {
+    //             url: '/apart',
+    //             templateUrl: 'p_applyformpage/p_apartpage/apart.html',
+    //             controller: 'apartCtrl'
+    //         }).state('/project', {
+    //             url: '/project',
+    //             templateUrl: 'p_applyformpage/p_projectpage/project.html',
+    //             controller: 'projectCtrl'
+    //         });
+    //         document.write("<h1>Hello</h1>");
+    //     //<![CDATA[
+    //     //document.write("<script async src='p_applyformpage/p_apartpage/apartCtrl.js'></script>".replace("HOST", location.hostname));
+    //     //document.write("<script async src='p_applyformpage/p_projectpage/projectCtrl.js'></script>".replace("HOST", location.hostname));
+    //     //]]>
+    //     $urlRouterProvider.otherwise('/applyform');
+    // }])
+    .controller('applyformCtrl', function ($scope, $state) {
+
+
+        $scope.greet = "申请填报主界面";
+        //返回主界面
+        $scope.gohome = function () {
+            $state.go('/homepage');
+
+        }
     
-    //跳项目出差申请
-    $scope.goProject = function () {
-        $state.go('/project');
+        //跳项目出差申请
+        $scope.goProject = function () {
+            $state.go('/project');
 
-    }
-    //跳项目出差申请
-    $scope.goSale = function () {
-        $state.go('/project');
+        }
+        //跳项目出差申请
+        $scope.goSale = function () {
+            $state.go('/project');
 
-    }
-    //跳部门出差申请
-    $scope.goApart = function () {
-        $state.go('/apart');
-    }
-});
+        }
+        //跳部门出差申请
+        $scope.goApart = function () {
+            $state.go('/apart');
+        }
+    });
